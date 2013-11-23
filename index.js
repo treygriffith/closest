7d1f564b8157c16fc576a983b2657517d591b61f
Trey Griffith <trey.griffith@gmail.com>
2013-11-22 19:58:38 -0800

    refactor into a `while`

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 var matches = require('matches-selector')
 
 module.exports = function (element, selector, checkYoSelf, root) {
-  element = checkYoSelf ? element : element.parentNode
-  // Make sure `element != null`
-  // otherwise we get an illegal invocation.
-  // this happens for disconnected DOM fragments
-  if (!element)
-    return
+  element = checkYoSelf ? {parentNode: element} : element
 
   root = root || document
 
-  do {
+  // Make sure `element !== document` and `element != null`
+  // otherwise we get an illegal invocation
+  while ((element = element.parentNode) && element !== document) {
     if (matches(element, selector))
       return element
     // After `matches` on the edge case that
     // the selector matches the root
     // (when the root is not the document)
     if (element === root)
-      return
-    // Make sure `element !== document`
-    // otherwise we get an illegal invocation
-  } while ((element = element.parentNode) && element !== document)
+      return  
+  }
 }
